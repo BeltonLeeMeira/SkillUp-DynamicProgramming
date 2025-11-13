f3fa97fad663b4c2b62b618f2a826b3a60b08f77
Belton Meira <blcdmmeira@gmail.com>
2025-11-13 12:09:50 -0300

    funcao auxiliar merge()

--- dynamic_skillup.py
+++ dynamic_skillup.py
@@ -43,3 +43,18 @@ def analisar_profissionais():
         direita = merge_sort(lista[meio:])
 
         return merge(esquerda, direita)
+
+    # Função auxiliar do Merge Sort
+    def merge(esquerda, direita):
+        resultado = []
+        i = j = 0
+        while i < len(esquerda) and j < len(direita):
+            if esquerda[i]["pontuacao"] > direita[j]["pontuacao"]:
+                resultado.append(esquerda[i])
+                i += 1
+            else:
+                resultado.append(direita[j])
+                j += 1
+        resultado.extend(esquerda[i:])
+        resultado.extend(direita[j:])
+        return resultado
